637448c3e8bf8b47b1d35558b1bc1dc9079209f2
guaracyalima <guaracyaraujolima@gmail.com>
2017-04-18 14:32:34 -0300

    Seeder users

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
 
 class UsersTableSeeder extends Seeder
@@ -11,6 +12,27 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        factory(User::class)->create([
+            'name' => 'User Common',
+            'email' => 'user@user.com',
+            'password' => bcrypt(123456),
+            'remember_token' => str_random(10)]);
+
+        factory(User::class)->create([
+            'name' => 'Admin root',
+            'email' => 'root@user.com',
+            'password' => bcrypt(123456),
+            'role' => 'root',
+            'remember_token' => str_random(10)]);
+
+        factory(User::class)->create([
+            'name' => 'Rikudou Hakernin Rootolino da Silva',
+            'email' => 'rikudou@user.com',
+            'password' => bcrypt(123456),
+            'role' => 'admin',
+            'remember_token' => str_random(10)]);
+
+
+
     }
 }
